742736b31aab1934e106d1fc887d06f432f8fb78
Dillon Ellis <dillonzellis@gmail.com>
2022-11-01 21:26:01 -0400

    add sass infrastructure

--- GBD_Theme/header.php
+++ GBD_Theme/header.php
@@ -28,7 +28,8 @@
     <?php wp_head(); ?>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap"
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Raleway:wght@400;600;700&display=swap"
         rel="stylesheet">
 </head>
 
