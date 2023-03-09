78cce3e1398b60fc9b1d87e83a42438a951d43e6
Dillon Ellis <dellis@fcul.com>
2023-03-09 16:17:46 -0500

    search bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 // console.log('working');
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+const searchBtn = document.querySelector("#search-icon");
+
+searchBtn.addEventListener("click", () => {
+  const searchBar = document.querySelector(".search-bar-container");
+  searchBar.classList.toggle("search-active");
+});
