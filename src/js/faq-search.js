20afff6f85f6b3915ea88c74ea903ee69a24911f
Dillon Ellis <dillonzellis@gmail.com>
2023-04-28 10:40:53 -0400

    force input styles

--- src/js/faq-search.js
+++ src/js/faq-search.js
@@ -1,45 +1,72 @@
 const searchInput = document.getElementById("search-input");
-const cards = document.querySelectorAll(".card-de");
+const loanTitle = document.getElementById("loan-title");
+const resultsFail = document.getElementById("results-fail");
 
-// two different arrays generalQuestions and loanQuestions
-// loop through generalQuestions and loanQuestions and push to cards array
+const generalQA = document.querySelector("#generalQA");
+const loanQA = document.querySelector("#loanQA");
 
-// when input is empty remove matchCards
-// then add back generalQuestions and loanQuestions arrays like they were originally
+const generalCards = generalQA.querySelectorAll(".card-de");
+const loanCards = loanQA.querySelectorAll(".card-de");
 
-const matchedCards = [];
+const generalQuestions = [];
+const loanQuestions = [];
+
+generalCards.forEach((card) => {
+  generalQuestions.push(card);
+});
+
+loanCards.forEach((card) => {
+  loanQuestions.push(card);
+});
 
 searchInput.addEventListener("input", () => {
   const searchValue = searchInput.value.toLowerCase();
 
-  cards.forEach((card) => {
+  generalCards.forEach((card) => {
     const cardContent = card.textContent.toLowerCase();
     if (cardContent.includes(searchValue)) {
-      matchedCards.push(card);
+      card.style.display = "block";
     } else {
       card.style.display = "none";
     }
   });
 
-  //loop through matched cards add cards to card-row class as children
-  matchedCards.forEach((card) => {
-    const cardRow = document.querySelector(".card-row");
-    cardRow.appendChild(card);
-
-    //hide loan-title
-    // const loanTitle = document.querySelector(".loan-title");
-    // loanTitle.style.display = "none";
-
-    //reset page to where it was before
-    if (searchValue === "") {
-      cards.forEach((card) => {
-        card.style.display = "block";
-      });
+  loanCards.forEach((card) => {
+    const cardContent = card.textContent.toLowerCase();
+    if (cardContent.includes(searchValue)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
     }
   });
 
-  //if card-row has no children remove card-row
-  // if (cardRow.children.length === 0) {
-  //   cardRow.remove();
-  // }
+  // if no matches show message in generalQuestions and no matches found in loanQuestions show message
+  if (generalQuestions.every((card) => card.style.display === "none") && loanQuestions.every((card) => card.style.display === "none")) {
+    loanTitle.style.display = "none";
+    // add class to resultsFail
+    resultsFail.classList.add("block-f");
+    // generalQA.innerHTML = `<p class="fail-msg text-center">No matches found</p>`;
+  }
+
+  // if matches only in generalQuestions hide loanTitle
+  if (generalQuestions.some((card) => card.style.display === "block") && loanQuestions.every((card) => card.style.display === "none")) {
+    loanTitle.style.display = "none";
+  }
+
+  // if any matches remove class from resultsFail
+  if (generalQuestions.some((card) => card.style.display === "block") || loanQuestions.some((card) => card.style.display === "block")) {
+    resultsFail.classList.remove("block-f");
+  }
+
+  // reset page to where it was before
+  if (searchValue === "") {
+    resultsFail.classList.remove("block-f");
+    generalCards.forEach((card) => {
+      card.style.display = "block";
+    });
+    loanTitle.style.display = "block";
+    loanCards.forEach((card) => {
+      card.style.display = "block";
+    });
+  }
 });
