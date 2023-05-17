bffd31ab535b8b18e9d4f4e1550e5bbf0e63bcc6
Dillon Ellis <dillonzellis@gmail.com>
2023-05-17 12:11:31 -0400

    update function file to remove divi zoom disable

--- GBD_Theme/functions.php
+++ GBD_Theme/functions.php
@@ -1,8 +1,24 @@
 <?php
-function gbd_equeue_scripts(){
-    wp_enqueue_style('parent-style', get_stylesheet_directory_uri(). '/dist/main.css');
-    wp_enqueue_script('theme-js', get_stylesheet_directory_uri() . '/dist/main.js', array(), null, true);
-    
+
+//Remove divi zoom disable
+function override_et_add_viewport_meta()
+{
+    remove_action('wp_head', 'et_add_viewport_meta');
+    add_action('wp_head', 'et_add_viewport_meta_child');
 }
 
-add_action('wp_enqueue_scripts', 'gbd_equeue_scripts' );
+function et_add_viewport_meta_child()
+{
+    echo '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=1" />';
+}
+
+add_action('after_setup_theme', 'override_et_add_viewport_meta');
+
+// GBD Theme files
+function gbd_equeue_scripts()
+{
+    wp_enqueue_style('parent-style', get_stylesheet_directory_uri() . '/dist/main.css');
+    wp_enqueue_script('theme-js', get_stylesheet_directory_uri() . '/dist/main.js', array(), null, true);
+}
+
+add_action('wp_enqueue_scripts', 'gbd_equeue_scripts');
