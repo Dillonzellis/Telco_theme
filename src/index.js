ef40902947a1500b5adc6f573ec11d71610043c1
Dillon Ellis <dillonzellis@gmail.com>
2023-05-17 13:40:00 -0400

    remove faq js from theme js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import "./main.scss";
 import "./js/current-year";
 import "./js/custom-nav";
 import "./js/espBtn-dropDown";
-import "./js/faq-search";
+// import "./js/faq-search";
 // import "./js/top-search-bar";
